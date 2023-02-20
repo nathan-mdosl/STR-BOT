f83c426ab4baa465202c7f9f16cf79d4a3d36f65
Nathan De Lima <n.de-lima@rgu.ac.uk>
2023-02-20 20:17:21 +0530

    deleted

--- testcases/test3.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-import datetime
-
-today = str(datetime.datetime.now().isoformat(sep="-",timespec="seconds"))
-
-s = today
-s = s.replace("/", "_").replace(":", "-")
-print(s)
